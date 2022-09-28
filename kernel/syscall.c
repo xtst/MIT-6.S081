8a4fcdbccafc1f2834c6c5cf9e45b9579e99354e
otnasxin@163.com <xintongsanto@yeah.net>
2022-09-29 00:35:06 +0800

    syscall part 1 done

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -120,6 +120,31 @@ static uint64 (*syscalls[])(void) = {
 	[SYS_trace] sys_trace,
 };
 
+static char sysnames[35][9] = {
+	[SYS_fork] "fork",
+	[SYS_exit] "exit",
+	[SYS_wait] "wait",
+	[SYS_pipe] "pipe",
+	[SYS_read] "read",
+	[SYS_kill] "kill",
+	[SYS_exec] "exec",
+	[SYS_fstat] "fstat",
+	[SYS_chdir] "chdir",
+	[SYS_dup] "dup",
+	[SYS_getpid] "getpid",
+	[SYS_sbrk] "sbrk",
+	[SYS_sleep] "sleep",
+	[SYS_uptime] "uptime",
+	[SYS_open] "open",
+	[SYS_write] "write",
+	[SYS_mknod] "mknod",
+	[SYS_unlink] "unlink",
+	[SYS_link] "link",
+	[SYS_mkdir] "mkdir",
+	[SYS_close] "close",
+	[SYS_trace] "trace",
+};
+
 void syscall(void) {
 	int num;
 	struct proc *p = myproc();
@@ -127,12 +152,12 @@ void syscall(void) {
 	num = p->trapframe->a7;
 	if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
 		p->trapframe->a0 = syscalls[num]();
+		if (((p->systrace) >> (num)) & 1) {
+			printf("%d: syscall %s -> %d\n", p->pid, sysnames[num], p->trapframe->a0);
+		}
 	} else {
 		printf("%d %s: unknown sys call %d\n",
 			   p->pid, p->name, num);
 		p->trapframe->a0 = -1;
-	}
-	if (((p->systrace) >> (num)) & 1) {
-		printf("%d: syscall %s -> %d\n", p->pid, p->name, p->trapframe->a0);
 	}
 }
