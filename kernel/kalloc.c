90d9b8316cba3221bff70c942b77ec6d630b85ea
otnasxin@163.com <xintongsanto@yeah.net>
2022-11-05 22:38:54 +0800

    lazy allocation done but lock and copy undone

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -11,72 +11,76 @@
 
 void freerange(void *pa_start, void *pa_end);
 
+struct spinlock page_count_lock;
+int page_count[(PHYSTOP - KERNBASE) / PGSIZE];
+
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+				   // defined by kernel.ld.
+
+struct spinlock page_count_lock;
 
 struct run {
-  struct run *next;
+	struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+	struct spinlock lock;
+	struct run *freelist;
 } kmem;
 
-void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+void kinit() {
+	initlock(&kmem.lock, "kmem");
+	initlock(&page_count_lock, "page count lock");
+	freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
-{
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+void freerange(void *pa_start, void *pa_end) {
+	char *p;
+	p = (char *)PGROUNDUP((uint64)pa_start);
+	for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE) { kfree(p); }
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
-{
-  struct run *r;
+void kfree(void *pa) {
+	struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+	acquire(&page_count_lock);
+	if (--page_count[PA2COUNT(pa)] != 0) return;
+	release(&page_count_lock);
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+	if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+		panic("kfree");
 
-  r = (struct run*)pa;
+	// Fill with junk to catch dangling refs.
+	memset(pa, 1, PGSIZE);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+	r = (struct run *)pa;
+	acquire(&kmem.lock);
+	r->next = kmem.freelist;
+	kmem.freelist = r;
+	release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
-  struct run *r;
+kalloc(void) {
+	struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+	acquire(&kmem.lock);
+	r = kmem.freelist;
+	if (r)
+		kmem.freelist = r->next;
+	release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+	if (r)
+		memset((char *)r, 5, PGSIZE); // fill with junk
+	acquire(&page_count_lock);
+	page_count[PA2COUNT(r)]++;
+	release(&page_count_lock);
+	return (void *)r;
 }
