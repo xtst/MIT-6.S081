e08b0946a2ee3eac7aeee9c3a3a611806d57fdee
otnasxin@163.com <xintongsanto@yeah.net>
2022-11-09 16:49:53 +0800

    thread lab done

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -28,6 +28,15 @@ barrier() {
 	// Block until all threads have called barrier() and
 	// then increment bstate.round.
 	//
+	pthread_mutex_lock(&bstate.barrier_mutex);
+	if (++bstate.nthread < nthread) {
+		pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+	} else {
+		bstate.round++;
+		bstate.nthread = 0;
+		pthread_cond_broadcast(&bstate.barrier_cond);
+	}
+	pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
